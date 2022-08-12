679e57e93bbeaf97e82d84795bee0c2d52d592b4
unknown <buragohain4@gmail.com>
2022-08-12 19:17:55 +0530

    Implemented UC2

---
+++ UC2/UC2/LinkedList.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UC2
+{
+    public class LinkedList
+    {
+        internal Node head;
+        internal void Add(int data)
+        {
+            Node node = new Node(data);
+            if (this.head == null)
+            {
+                this.head = node;
+            }
+            else
+            {
+                Node temp = head;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = node;
+            }
+            Console.WriteLine("{0}, insterted into Linked List", node.data);
+        }
+        internal void Display()
+        {
+            Node temp = this.head;
+            if (temp == null)
+            {
+                Console.WriteLine("Empty LinkedList");
+                return;
+            }
+            while (temp != null)
+            {
+                Console.Write(temp.data + " -> ");
+                temp = temp.next;
+            }
+        }
+    }
+}
---
+++ UC2/UC2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace UC2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("LinkedList");
+            LinkedList List = new LinkedList();
+            List.Add(70);
+            List.Add(30);
+            List.Add(56);
+            List.Display();
+        }
+    }
+}
