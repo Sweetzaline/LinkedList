27a0f85f43724fbd356ff0303ea0dcae4e775dbb
unknown <buragohain4@gmail.com>
2022-08-12 18:42:48 +0530

    Implemented UC1

---
+++ UC1/UC1/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+namespace UC1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("LinkedList");
+            LinkedList List = new LinkedList();
+            List.Add(56);
+            List.Add(30);
+            List.Add(70);
+            List.Display();
+        }
+    }
+    public class Node
+    {
+        public int data;
+        public Node next;
+        public Node(int data)
+        {
+            this.data = data;
+        }
+    }
+    public class LinkedList
+    {
+        internal Node head;
+        internal void Add(int data)
+        {
+            Node node = new Node(data);
+            if (this.head == null)
+            {
+                this.head = node;
+            }
+            else
+            {
+                Node temp = head;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = node;
+            }
+            Console.WriteLine("{0}, insterted into Linked List", node.data);
+        }
+        internal void Display()
+        {
+            Node temp = this.head;
+            if (temp == null)
+            {
+                Console.WriteLine("Empty LinkedList");
+                return;
+            }
+            while (temp != null)
+            {
+                Console.WriteLine(temp.data + " ");
+                temp = temp.next;
+            }
+        }
+    }   
+}
